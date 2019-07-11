9bdc4e14f8fb80f7e52bb62c6d44eeb11e228e28
Alberto Zepeda <aaze92@gmail.com>
2019-07-11 01:07:50 -0500

    indentacion

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,15 @@
 //===================================
 //          PUERTO
 //===================================
-
 process.env.PORT = process.env.PORT || 3000;
-
 //===================================
 //          ENTORNO
 //===================================
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
-
 let urlDB;
-
 if (process.env.NODE_ENV ==='dev'){
     urlDB='mongodb://localhost:27017/mariscos';
 }else{
     urlDB=process.env.MONGO_URI;
 }
-
-
 process.env.URLDB = urlDB;
-
-//===================================
-//          ENTORNO
-//===================================
